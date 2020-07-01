d6afccb82a90b07830de2ec969ca3a5f8638b675
DEV-MatheusOFC <matheusnunesoliveira198@gmail.com>
2020-07-01 10:07:33 -0300

    Aula Finalizada

---
+++ Produto.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Aula_27_28_29_30
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        private const string PATH = "Database/produto.csv";
+
+        public string Separar(string dado){
+            return dado.Split('=')[1];
+        }
+        public Produto(){
+            // Cria o arquivo caso não exista
+            if(!File.Exists(PATH)){
+                Directory.CreateDirectory("Database");
+                File.Create(PATH).Close();
+            }
+        }
+
+        public void Cadastrar(Produto prod){
+            var linha = new string[] {PrepararLinha(prod)};
+            File.AppendAllLines(PATH, linha);
+        }
+
+        public List<Produto> Ler(){
+            List<Produto> produtos = new List<Produto>();
+
+            string[] linhas = File.ReadAllLines(PATH);
+
+            foreach (var linha in linhas){
+                string [] dados = linha.Split(';');
+
+                Produto prod = new Produto();
+                prod.Codigo  = Int32.Parse(Separar(dados[0]));
+                prod.Nome    = Separar(dados[1]);
+                prod.Preco   = float.Parse( Separar(dados[2]));
+
+                produtos.Add(prod);
+            }
+            
+            produtos = produtos.OrderBy(z => z.Nome).ToList();
+
+            return produtos;
+        }
+
+        public List<Produto> Filtrar(string _nome){
+            return Ler().FindAll(n => n.Nome == _nome);
+        }
+        public void Remover(string _termo){
+
+            List<string> linhas = new List<string>();
+            
+            using(StreamReader arquivo = new StreamReader(PATH)){
+
+                string linha;
+
+                while((linha = arquivo.ReadLine()) != null ){
+                    linhas.Add(linha);
+                }
+
+                linhas.RemoveAll(z => z.Contains(_termo));
+            }
+
+            using(StreamWriter output = new StreamWriter(PATH)){
+
+                output.Write(String.Join(Environment.NewLine, linhas.ToArray())); 
+
+            }
+        }
+        private string PrepararLinha(Produto p){
+            return $"\nCodigo={p.Codigo};nome={p.Nome};preco={p.Preco}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula_27_28_29_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto();
+            p1.Codigo  = 1;
+            p1.Nome    = "Galaxy A50";
+            p1.Preco   = 1599f;
+
+            p1.Cadastrar(p1);
+            p1.Remover("1799");
+
+            List<Produto> lista = p1.Ler();
+
+            foreach (Produto item in lista){
+               System.Console.WriteLine($"R${item.Preco} - {item.Nome}");
+            }
+
+        }
+    }
+}
